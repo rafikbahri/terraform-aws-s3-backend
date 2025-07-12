dbccffdf1169e0507371d33a560f23d87983e93f
Rafik Bahri <rbahri@feedgy.solar>
2025-07-12 23:51:56 +0200

    feat: Add output backend config

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "tf_lock_dynamodb_table_name" {
   description = "DynamoDB table name for Terraform state locking"
   value       = aws_dynamodb_table.terraform_locks.name
 }
+
+output "tf_backend_config_string" {
+  description = "Terraform backend configuration as a formatted string"
+  value       = <<EOT
+terraform {
+  backend "s3" {
+    bucket         = "${aws_s3_bucket.terraform_state.id}"
+    key            = "terraform.tfstate"
+    region         = "${aws_s3_bucket.terraform_state.region}"
+    dynamodb_table = "${aws_dynamodb_table.terraform_locks.name}"
+    encrypt        = true
+  }
+}
+EOT
+}
